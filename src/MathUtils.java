eaedb0199e60ef11525e14ad50760eb906763cf2
pvarenik <varenikpavel@gmail.com>
2018-02-11 11:42:52 +0300

    Path (parent directories test fails)

---
+++ src/MathUtils.java
@@ -0,0 +1,9 @@
+public class MathUtils {
+    public static double average(double a, double b) {
+        return ((a + b) / 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(2, 1));
+    }
+}
